9df054b2fc09897e27cc3255f93ac7dc74867f81
gianets <giancarlo.rumachella@gmail.com>
2021-03-23 20:00:31 -0300

    refactor da interface

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,6 +7,124 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  _calcButton({String texto}) {
+    return Expanded(
+      flex: 1,
+      child: RaisedButton(
+        onPressed: () {
+          print("botão pressionado");
+        },
+        child: Text(
+          texto,
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      ),
+    );
+  }
+
+  _createDisplay() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AutoSizeText(
+                "000",
+                minFontSize: 20.0,
+                maxFontSize: 80.0,
+                maxLines: 1,
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  decoration: TextDecoration.none,
+                  fontSize: 80.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _createKeyboard() {
+    return Expanded(
+      child: Container(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _calcButton(texto: "C"),
+                  _calcButton(texto: "+/-"),
+                  _calcButton(texto: "%"),
+                  _calcButton(texto: "DEL"),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _calcButton(texto: "7"),
+                  _calcButton(texto: "8"),
+                  _calcButton(texto: "9"),
+                  _calcButton(texto: "/"),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _calcButton(texto: "4"),
+                  _calcButton(texto: "5"),
+                  _calcButton(texto: "6"),
+                  _calcButton(texto: "X"),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _calcButton(texto: "1"),
+                  _calcButton(texto: "2"),
+                  _calcButton(texto: "3"),
+                  _calcButton(texto: "-"),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _calcButton(texto: "0"),
+                  _calcButton(texto: "."),
+                  _calcButton(texto: "="),
+                  _calcButton(texto: "+"),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,320 +133,14 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.grey[400],
         title: Text("Calculadora"),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            TextField(
-              style: TextStyle(fontSize: 100),
-            ),
-            Divider(
-              height: 1.5,
-              color: Colors.grey[900],
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "C",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "+/-",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "%",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "DEL",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            ),
-            Divider(
-              height: 1.5,
-              color: Colors.grey[900],
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "7",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "8",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "9",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "/",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            ),
-            Divider(
-              height: 1.5,
-              color: Colors.grey[900],
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "4",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "5",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "6",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "X",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            ),
-            Divider(
-              height: 1.5,
-              color: Colors.grey[900],
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "1",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "2",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "3",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "-",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            ),
-            Divider(
-              height: 1.5,
-              color: Colors.grey[900],
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "0",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      ".",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "=",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                  Divider(
-                    thickness: 1.5,
-                    color: Colors.grey[900],
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "+",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.normal),
-                    ),
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            ),
-            Divider(
-              height: 1.5,
-              color: Colors.grey[900],
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          _createDisplay(),
+          Divider(
+            height: 0.5,
+          ),
+          _createKeyboard(),
+        ],
       ),
     );
   }
